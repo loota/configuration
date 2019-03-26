d42280072d46044e4f89ebb96ff24c965a79a210
Loota <foo@foo>
2019-03-26 19:30:55 +0200

    Map escape to control

--- xcape.sh
+++ xcape.sh
@@ -2,3 +2,15 @@ xcape -e "Control_L=Control_R|Super_L|Left"
 xcape -e "Control_R=Control_R|Super_L|Right"
 xcape -e "Shift_R=Shift_R|Alt_L|Tab"     
 xcape -e "Shift_L=Alt_L|Tab"
+
+spare_modifier="Hyper_L"
+
+xmodmap -e "keycode 66 = $spare_modifier"
+xmodmap -e "remove mod4 = $spare_modifier"
+xmodmap -e "add Control = $spare_modifier"
+
+xmodmap -e "keycode any = 9"
+
+xcape -e "$spare_modifier=Escape"
+
+
